f4255ee3514479bb108294d4a94b2161047a556b
Nirajan Basnet <er.nirajan.basnet@gmail.com>
2019-07-12 14:48:58 +0545

    Setup

--- themes/yi-hugo/src/js/tailwind.config.js
+++ themes/yi-hugo/src/js/tailwind.config.js
@@ -1,5 +1,3 @@
-const { colors } = require('tailwindcss/defaultTheme')
-
 module.exports = {
   theme: {
     screens: {
@@ -10,22 +8,8 @@ module.exports = {
       xll: '1400px',
     },
     fontFamily: {
-      body: ['Fira Sans', 'sans-serif'],
-    },
-    colors: {
-      white: '#fff',
-      'grey': '#565656',
-      'base-color': 'var(--color-text-base-color)',
-      'primary-background': "var(--color-bg-primary)",
-      'primary-text': "var(--color-text-primary)",
-      'base-link': "var(--color-base-link)",
-    },
-    extend: {
-      spacing: {
-        '96': '24rem',
-        '128': '32rem',
-      }
-    },
+      body: ['Poppins', 'sans-serif'],
+    }
   },
   variants: {},
   plugins: []
